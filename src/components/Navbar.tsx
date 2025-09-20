1d112bb199b3c4a6b8d7850b4e0ef17ba7faa8c1
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-20 22:50:09 +0530

    style(Navbar): update link styles and adjust key display format

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,20 +34,22 @@ export default function Navbar() {
     <nav className="flex max-w-3xl mx-auto pb-8 text-sm text-muted-foreground items-center gap-4">
       {links.map(({ href, text, key }) => (
         <h4
-          className="px-2 py-2 hover:text-blue-500 sm:text-lg text-xs duration-300 transition-colors"
+          className="hover:text-blue-500 sm:text-sm text-xs duration-300 transition-colors"
           key={key}
         >
           <Link href={href}>
             <span
-              className={`${pathname === href
-                  ? "bg-blue-500 text-white px-2 py-1 rounded"
-                  : pathname.startsWith("/blogs") && href === "/blogs"
-                    ? "bg-blue-500 text-white px-2 py-1 rounded"
-                    : ""
-                }`}
+              className={`${
+                pathname === href
+                  ? "text-blue-500"
+                  : pathname.startsWith("/w") && href === "/w"
+                  ? "text-blue-500"
+                  : ""
+              }`}
             >
-              {text}
-            </span>
+              [{key}]
+            </span>{" "}
+            {text}
           </Link>
         </h4>
       ))}
