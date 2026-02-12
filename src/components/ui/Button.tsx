070b78a2ebecf04d78898e765b67068c3acace6a
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-12 20:45:10 +0530

    [src] remove: src/components/ui/Button.tsx

--- src/components/ui/Button.tsx
+++
@@ -1,7 +0,0 @@
-export const Button = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button className="bg-yellow-500 px-4 py-2 rounded text-white">
-      {children}
-    </button>
-  );
-};
