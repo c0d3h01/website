dac4971b363deb10c89a981d8e9e5ffa20255b42
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-12 20:45:08 +0530

    [src] remove: src/components/Projects/UserCount.tsx

--- src/components/Projects/UserCount.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function UserCount({ count }: { count: number }) {
-  return (
-    <div className="text-xs border px-2 py-1 border-orange-400 text-orange-500 bg-orange-600/10 select-none hover:bg-orange-600/20 transition-colors">
-      {count}+ Users
-    </div>
-  );
-}
