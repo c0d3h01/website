7729c5e565698d02ab53dde6fb31ff17c88aca52
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-12 20:44:23 +0530

    [root] update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "huggingfacefw-blogpost-fineweb-v1.static.hf.space",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {}
 
-export default nextConfig;
+export default nextConfig
