edea4375238db24585cf7a7d62393fb8dba056ec
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-12 20:45:00 +0530

    [src] remove: src/components/H2Heading.tsx

--- src/components/H2Heading.tsx
+++
@@ -1,6 +0,0 @@
-import { Star } from "@/components/Star";
-import React from "react";
-
-export function H2Heading({ children }: { children: React.ReactNode }) {
-  return <h2 className="text-xl font-semibold text-stone-50/70"><Star/> {children}</h2>;
-}
