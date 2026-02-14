79679a571a40b026c9e293b0c4557c1c38f74d77
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-14 13:19:54 +0530

    [src] feat: Remove GitHub contributions section and `react-activity-calendar` dependency.

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -5,7 +5,6 @@ import HireSection from "@/components/home/HireSection"
 import ProfileHeader from "@/components/home/ProfileHeader"
 import ProjectsSection from "@/components/home/ProjectsSection"
 import SocialSection from "@/components/home/SocialSection"
-import GitHubContributions from "@/components/home/GitHubContributions"
 import SkillsSection from "@/components/home/SkillsSection"
 import SupportSection from "@/components/home/SupportSection"
 import WritingsSection from "@/components/home/WritingsSection"
@@ -24,7 +23,6 @@ const HomePage = () => {
         />
         <AboutSection />
         <SocialSection />
-        <GitHubContributions />
         <ExperienceSection />
         <HireSection />
         <SkillsSection />
