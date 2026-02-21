b3dc0039e2dab4c310be42f4f1e65be32b3593e6
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-21 22:12:34 +0530

    chore: update src/components/home/SupportSection.tsx

--- src/components/home/SupportSection.tsx
+++ src/components/home/SupportSection.tsx
@@ -1,45 +1,38 @@
+import ButtonLink from "@/components/ui/ButtonLink"
 import SectionHeading from "@/components/ui/SectionHeading"
 import SupportCopyButton from "@/components/ui/SupportCopyButton"
 import { supportMethods, supportText } from "@/data"
 
 const SupportSection = () => {
-  const renderSupportAction = (method: (typeof supportMethods)[number]) => {
-    const Icon = method.icon
-
-    if (method.type === "link") {
-      return (
-        <a
-          key={method.id}
-          className="btn"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={method.href}
-        >
-          <Icon />
-          {method.label}
-        </a>
-      )
-    }
-
-    return (
-      <SupportCopyButton
-        key={method.id}
-        label={method.label}
-        value={method.value}
-        mobileHref={method.mobileHref}
-      >
-        <Icon />
-      </SupportCopyButton>
-    )
-  }
-
   return (
     <section className="flex flex-col gap-2">
       <SectionHeading title="Support Me" />
       <div className="section-copy flex flex-col gap-2.5">
         <p>{supportText}</p>
         <div className="flex flex-wrap items-center gap-2">
-          {supportMethods.map(renderSupportAction)}
+          {supportMethods.map((method) => {
+            const Icon = method.icon
+
+            if (method.type === "link") {
+              return (
+                <ButtonLink key={method.id} href={method.href}>
+                  <Icon />
+                  {method.label}
+                </ButtonLink>
+              )
+            }
+
+            return (
+              <SupportCopyButton
+                key={method.id}
+                label={method.label}
+                value={method.value}
+                mobileHref={method.mobileHref}
+              >
+                <Icon />
+              </SupportCopyButton>
+            )
+          })}
         </div>
       </div>
     </section>
