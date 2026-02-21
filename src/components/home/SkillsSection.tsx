92904a5bf1ec8277996e09b88f0f9c40f7f3c09a
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-21 22:12:34 +0530

    chore: update src/components/home/SkillsSection.tsx

--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -6,10 +6,10 @@ const SkillsSection = () => {
     <section className="flex flex-col gap-2">
       <SectionHeading title="Skills & Tools" />
       <div className="flex flex-wrap gap-1.5">
-        {skills.map((skill) => (
-          <span key={skill.id} className="skills-card">
-            <skill.icon />
-            {skill.text}
+        {skills.map(({ id, icon: Icon, text }) => (
+          <span key={id} className="skills-card">
+            <Icon />
+            {text}
           </span>
         ))}
       </div>
