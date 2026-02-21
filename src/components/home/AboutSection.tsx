46d2a2acd7500d7b5508e41180597cdef2d0b5e4
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-21 22:12:32 +0530

    chore: update src/components/home/AboutSection.tsx

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -2,12 +2,14 @@ import SectionHeading from "@/components/ui/SectionHeading"
 import { profile } from "@/data"
 
 const AboutSection = () => {
+  const aboutHtml = profile.aboutHtml
+
   return (
     <section className="flex flex-col gap-2">
       <SectionHeading title="About Me" />
       <div
         dangerouslySetInnerHTML={{
-          __html: profile.aboutHtml,
+          __html: aboutHtml,
         }}
         className="section-copy flex flex-col gap-2.5"
       />
