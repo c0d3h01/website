7305605675b21b0ae23672fc4c74b206f174d8f4
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:18 +0530

    chore: add src/components/home/GitHubProfileHeader.tsx

---
+++ src/components/home/GitHubProfileHeader.tsx
@@ -0,0 +1,17 @@
+import ProfileHeader from "@/components/home/ProfileHeader"
+import { profile, profileImage } from "@/data"
+import { getGitHubUser } from "@/lib/github"
+
+const GitHubProfileHeader = async () => {
+  const githubUser = await getGitHubUser(profile.githubUsername)
+
+  return (
+    <ProfileHeader
+      userName={profile.name}
+      userBio={profile.bio}
+      userImage={githubUser?.avatar_url ?? profileImage}
+    />
+  )
+}
+
+export default GitHubProfileHeader
