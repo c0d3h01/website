bc530408e14b3a63e04e212d5e7b6771c5fd977c
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:16 +0530

    chore: update src/components/home/HireSection.tsx

--- src/components/home/HireSection.tsx
+++ src/components/home/HireSection.tsx
@@ -1,27 +1,27 @@
-import { PiFilePdfBold, PiSuitcaseSimpleBold } from "react-icons/pi"
-import ButtonLink from "@/components/ui/ButtonLink"
-import SectionHeading from "@/components/ui/SectionHeading"
-import { emailLink, hireText, resumeFilePath } from "@/data"
+import { PiFilePdfBold, PiSuitcaseSimpleBold } from "react-icons/pi";
+import ButtonLink from "@/components/ui/ButtonLink";
+import SectionHeading from "@/components/ui/SectionHeading";
+import { emailLink, hireText, resumeFilePath } from "@/data";
 
 const HireSection = () => {
-  return (
-    <section className="flex flex-col gap-2">
-      <SectionHeading title="Hire Me" />
-      <div className="section-copy flex flex-col gap-2.5">
-        <p>{hireText}</p>
-        <div className="flex flex-wrap items-center gap-2.5">
-          <ButtonLink href={emailLink}>
-            <PiSuitcaseSimpleBold />
-            Hire Me
-          </ButtonLink>
-          <ButtonLink href={resumeFilePath}>
-            <PiFilePdfBold />
-            Resume
-          </ButtonLink>
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="flex flex-col gap-2">
+			<SectionHeading title="Hire Me" />
+			<div className="section-copy flex flex-col gap-2.5">
+				<p>{hireText}</p>
+				<div className="flex flex-wrap items-center gap-2.5">
+					<ButtonLink href={emailLink}>
+						<PiSuitcaseSimpleBold />
+						Hire Me
+					</ButtonLink>
+					<ButtonLink href={resumeFilePath}>
+						<PiFilePdfBold />
+						Resume
+					</ButtonLink>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default HireSection
+export default HireSection;
