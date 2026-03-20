68bd6cc6885d3029cc8bd690b5187905a0cdb0ee
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:16 +0530

    chore: update src/components/home/ProjectsSection.tsx

--- src/components/home/ProjectsSection.tsx
+++ src/components/home/ProjectsSection.tsx
@@ -1,19 +1,19 @@
-import ProjectList from "@/components/projects/ProjectList"
-import SectionHeading from "@/components/ui/SectionHeading"
-import ViewAllLink from "@/components/ui/ViewAllLink"
-import { projects } from "@/data"
+import ProjectList from "@/components/projects/ProjectList";
+import SectionHeading from "@/components/ui/SectionHeading";
+import ViewAllLink from "@/components/ui/ViewAllLink";
+import { projects } from "@/data";
 
 const ProjectsSection = () => {
-  const visibleCount = 2
-  const shouldShowViewAll = projects.length > visibleCount
+	const visibleCount = 2;
+	const shouldShowViewAll = projects.length > visibleCount;
 
-  return (
-    <section id="projects" className="flex flex-col gap-3">
-      <SectionHeading title="Projects" />
-      <ProjectList items={projects} limit={visibleCount} />
-      {shouldShowViewAll && <ViewAllLink href="/projects" />}
-    </section>
-  )
-}
+	return (
+		<section id="projects" className="flex flex-col gap-3">
+			<SectionHeading title="Projects" />
+			<ProjectList items={projects} limit={visibleCount} />
+			{shouldShowViewAll && <ViewAllLink href="/projects" />}
+		</section>
+	);
+};
 
-export default ProjectsSection
+export default ProjectsSection;
