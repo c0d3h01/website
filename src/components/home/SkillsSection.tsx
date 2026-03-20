3ab8d110d4cc765a17763cb4c311196d85e1cb3e
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:16 +0530

    chore: update src/components/home/SkillsSection.tsx

--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -1,20 +1,20 @@
-import SectionHeading from "@/components/ui/SectionHeading"
-import { skills } from "@/data"
+import SectionHeading from "@/components/ui/SectionHeading";
+import { skills } from "@/data";
 
 const SkillsSection = () => {
-  return (
-    <section className="flex flex-col gap-2">
-      <SectionHeading title="Skills & Tools" />
-      <div className="flex flex-wrap gap-1.5">
-        {skills.map(({ id, icon: Icon, text }) => (
-          <span key={id} className="skills-card">
-            <Icon />
-            {text}
-          </span>
-        ))}
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="flex flex-col gap-2">
+			<SectionHeading title="Skills & Tools" />
+			<div className="flex flex-wrap gap-1.5">
+				{skills.map(({ id, icon: Icon, text }) => (
+					<span key={id} className="skills-card">
+						<Icon />
+						{text}
+					</span>
+				))}
+			</div>
+		</section>
+	);
+};
 
-export default SkillsSection
+export default SkillsSection;
