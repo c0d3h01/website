7d1199d9cd86a03aa9d4d818bfb50de7308d8857
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:17 +0530

    chore: update src/components/home/SocialSection.tsx

--- src/components/home/SocialSection.tsx
+++ src/components/home/SocialSection.tsx
@@ -1,21 +1,19 @@
-import ButtonLink from "@/components/ui/ButtonLink"
-import SectionHeading from "@/components/ui/SectionHeading"
-import { footerSocialLinks } from "@/data"
+import ButtonLink from "@/components/ui/ButtonLink";
+import { footerSocialLinks } from "@/data";
 
 const SocialSection = () => {
-  return (
-    <section className="flex flex-col gap-2.5">
-      <SectionHeading title="Social" />
-      <div className="flex flex-wrap items-center gap-2.5">
-        {footerSocialLinks.map((link) => (
-          <ButtonLink key={link.id} href={link.href}>
-            <link.icon />
-            {link.name}
-          </ButtonLink>
-        ))}
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="flex flex-col gap-2.5">
+			<div className="flex flex-wrap items-center gap-2.5">
+				{footerSocialLinks.map((link) => (
+					<ButtonLink key={link.id} href={link.href}>
+						<link.icon />
+						{link.name}
+					</ButtonLink>
+				))}
+			</div>
+		</section>
+	);
+};
 
-export default SocialSection
+export default SocialSection;
