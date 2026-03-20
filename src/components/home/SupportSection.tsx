bde386d884130e6b8bc66e193dea2cfa1e6a57c4
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:17 +0530

    chore: update src/components/home/SupportSection.tsx

--- src/components/home/SupportSection.tsx
+++ src/components/home/SupportSection.tsx
@@ -1,42 +1,42 @@
-import ButtonLink from "@/components/ui/ButtonLink"
-import SectionHeading from "@/components/ui/SectionHeading"
-import SupportCopyButton from "@/components/ui/SupportCopyButton"
-import { supportMethods, supportText } from "@/data"
+import ButtonLink from "@/components/ui/ButtonLink";
+import SectionHeading from "@/components/ui/SectionHeading";
+import SupportCopyButton from "@/components/ui/SupportCopyButton";
+import { supportMethods, supportText } from "@/data";
 
 const SupportSection = () => {
-  return (
-    <section className="flex flex-col gap-2">
-      <SectionHeading title="Support Me" />
-      <div className="section-copy flex flex-col gap-2.5">
-        <p>{supportText}</p>
-        <div className="flex flex-wrap items-center gap-2">
-          {supportMethods.map((method) => {
-            const Icon = method.icon
+	return (
+		<section className="flex flex-col gap-2">
+			<SectionHeading title="Support Me" />
+			<div className="section-copy flex flex-col gap-2.5">
+				<p>{supportText}</p>
+				<div className="flex flex-wrap items-center gap-2">
+					{supportMethods.map((method) => {
+						const Icon = method.icon;
 
-            if (method.type === "link") {
-              return (
-                <ButtonLink key={method.id} href={method.href}>
-                  <Icon />
-                  {method.label}
-                </ButtonLink>
-              )
-            }
+						if (method.type === "link") {
+							return (
+								<ButtonLink key={method.id} href={method.href}>
+									<Icon />
+									{method.label}
+								</ButtonLink>
+							);
+						}
 
-            return (
-              <SupportCopyButton
-                key={method.id}
-                label={method.label}
-                value={method.value}
-                mobileHref={method.mobileHref}
-              >
-                <Icon />
-              </SupportCopyButton>
-            )
-          })}
-        </div>
-      </div>
-    </section>
-  )
-}
+						return (
+							<SupportCopyButton
+								key={method.id}
+								label={method.label}
+								value={method.value}
+								mobileHref={method.mobileHref}
+							>
+								<Icon />
+							</SupportCopyButton>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default SupportSection
+export default SupportSection;
