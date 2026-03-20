f44ff754d452f6af98807f945ae1e6e4644fb675
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-20 23:24:16 +0530

    chore: update src/components/home/ExperienceSection.tsx

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -1,24 +1,24 @@
-import ExperienceList from "@/components/experience/ExperienceList"
-import SectionHeading from "@/components/ui/SectionHeading"
-import ViewAllLink from "@/components/ui/ViewAllLink"
-import { experiences } from "@/data"
+import ExperienceList from "@/components/experience/ExperienceList";
+import SectionHeading from "@/components/ui/SectionHeading";
+import ViewAllLink from "@/components/ui/ViewAllLink";
+import { experiences } from "@/data";
 
 const ExperienceSection = () => {
-  const activeExperiences = experiences.filter((item) =>
-    /present/i.test(item.duration),
-  )
-  const visibleExperiences =
-    activeExperiences.length > 0 ? activeExperiences : experiences.slice(0, 1)
+	const activeExperiences = experiences.filter((item) =>
+		/present/i.test(item.duration),
+	);
+	const visibleExperiences =
+		activeExperiences.length > 0 ? activeExperiences : experiences.slice(0, 1);
 
-  const shouldShowViewAll = experiences.length > visibleExperiences.length
+	const shouldShowViewAll = experiences.length > visibleExperiences.length;
 
-  return (
-    <section className="flex flex-col gap-3">
-      <SectionHeading title="Experience" />
-      <ExperienceList items={visibleExperiences} />
-      {shouldShowViewAll && <ViewAllLink href="/experience" />}
-    </section>
-  )
-}
+	return (
+		<section className="flex flex-col gap-3">
+			<SectionHeading title="Experience" />
+			<ExperienceList items={visibleExperiences} />
+			{shouldShowViewAll && <ViewAllLink href="/experience" />}
+		</section>
+	);
+};
 
-export default ExperienceSection
+export default ExperienceSection;
