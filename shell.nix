f981a61771262fb5b766b5a1cf7bf31ae83ac072
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-24 21:51:18 +0530

    refactor: switch dev shell dependencies to bun

--- shell.nix
+++ shell.nix
@@ -1,11 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   packages = [
-    pkgs.python3
-    pkgs.python3Packages.pip
-    # pkgs.python3Packages.venvShellHook
-    pkgs.uv
     pkgs.nodejs-slim_24
+    pkgs.bun
     pkgs.pnpm
     pkgs.typescript
     pkgs.typescript-language-server
